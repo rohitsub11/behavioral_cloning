3dce9425bc83b43e8326e197987ee7e3c979fff2
Rohit Subramanian <rohitsub11@gmail.com>
2017-01-22 09:53:49 -0800

    save model as json and weights as h5

--- model.py
+++ model.py
@@ -50,3 +50,11 @@ if __name__ == "__main__":
 	model = trial_model()
 
 	#model.fit_generator()
+
+	# serialize model to JSON
+	model_json = model.to_json()
+	with open("model.json", "w") as json_file:
+		json_file.write(model_json)
+    # serialize weights to HDF5
+	model.save_weights("model.h5")
+	print("Saved model to disk")
